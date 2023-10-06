7f239fa4ccda7b8562d849ddd67d84ec2cf051f5
Haamid Raza Kazi <haamidraza03@gmail.com>
2023-10-06 08:36:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Virtual Study Buddy Platform.
+    </div>
+  );
+}
+
+export default App;
