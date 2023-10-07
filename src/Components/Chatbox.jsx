c649a448abd7632484a73c04038ba28bd9e01704
Haamid Raza Kazi <haamidraza03@gmail.com>
2023-10-07 15:30:54 +0530

    lcomit

--- src/Components/Chatbox.jsx
+++ src/Components/Chatbox.jsx
@@ -8,14 +8,14 @@ function Chatbox() {
             ChatBox
         </div>
         <div className="row justify-content-around">
-            <div className="col-md-6 border hh">
+            <div className="col-md-4 border hh">
                 Students
             </div>
-            <div className="col-md-6 border bg-secondary hh text-white">
+            <div className="col-md-8 border bg-secondary hh text-white">
                 Chatting Interface
             </div>
         </div>
-        <input type="text" className='w-50' placeholder='Send Message' /> <IoSend/>
+        <input type="text" className='w-25 ms-4 rounded-pill' placeholder='Send Message' /> <span style={{cursor:"pointer"}}><IoSend/></span>
     </div>
   )
 }
